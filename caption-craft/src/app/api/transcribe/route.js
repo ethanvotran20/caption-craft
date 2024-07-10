32d9b7bbf919bccbaad4aadbd5a03e85dba75a82
ethanvotran20 <evotran@uci.edu>
2024-07-10 00:30:32 -0700

    testing git config user change

--- caption-craft/src/app/api/transcribe/route.js
+++ caption-craft/src/app/api/transcribe/route.js
@@ -66,6 +66,7 @@ async function getTranscriptionFile(filename) {
     }
 }
 
+
 export async function GET(req) {
     const url = new URL(req.url);
     const searchParams = new URLSearchParams(url.searchParams);
